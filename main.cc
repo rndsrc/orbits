ac78a2ec04a66b85ea88129a321720a67373dbee
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-08 12:15:00 -0400

    Parse the step size from the argument

--- main.cc
+++ main.cc
@@ -1,11 +1,17 @@
 #include "orbits.h"
 #include <cstring>
 #include <cstdio>
+#include <cstdlib>
 
 int main(int argc, char **argv)
 {
-	if(argc > 1 && !strcmp(argv[1], "--help"))
+	if(argc > 1 && !strcmp(argv[1], "--help")) {
 		printf("usage: %s [step_size] < input_file\n", argv[0]);
+		return 0;
+	}
+
+	real dt = argc > 1 ? atof(argv[1]) : 1e-3;
+	printf("dt = %g\n", dt);
 
 	return 0;
 }
