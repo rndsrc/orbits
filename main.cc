0b42a46f6fc0ac13cab59a797d0fd3d8782afb3f
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-08 14:22:09 -0400

    Move the numerical scheme into its own functions

--- main.cc
+++ main.cc
@@ -2,7 +2,36 @@
 #include <cstdlib>
 #include <cmath>
 
-const real eps = 1.0e-32;
+void acceleration(vector *a, particle *p, size_t n)
+{
+	const real eps = 1.0e-32;
+
+	for(size_t i = 0; i < n; ++i) {
+		a[i].z = a[i].y = a[i].x = 0.0;
+		for(size_t j = 0; j < n; ++j) {
+			real dx = p[j].r.x - p[i].r.x;
+			real dy = p[j].r.y - p[i].r.y;
+			real dz = p[j].r.z - p[i].r.z;
+			real rr = dx * dx + dy * dy + dz * dz + eps;
+			real f  = p[j].m / (rr * sqrt(rr));
+			a[i].x += f * dx;
+			a[i].y += f * dy;
+			a[i].z += f * dz;
+		}
+	}
+}
+
+void forwardEuler(particle *p, vector *a, size_t n, real dt)
+{
+	for(size_t i = 0; i < n; ++i) {
+		p[i].r.x += dt * p[i].u.x;
+		p[i].r.y += dt * p[i].u.y;
+		p[i].r.z += dt * p[i].u.z;
+		p[i].u.x += dt * a[i]  .x;
+		p[i].u.y += dt * a[i]  .y;
+		p[i].u.z += dt * a[i]  .z;
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -17,31 +46,8 @@ int main(int argc, char **argv)
 
 	vector *a = (vector *)malloc(sizeof(vector) * d->n);
 	for(size_t h = 0; h < 6283; ++h) {
-		size_t    n = d->n;
-		particle *p = d->p;
-
-		for(size_t i = 0; i < n; ++i) {
-			a[i].z = a[i].y = a[i].x = 0.0;
-			for(size_t j = 0; j < n; ++j) {
-				real dx = p[j].r.x - p[i].r.x;
-				real dy = p[j].r.y - p[i].r.y;
-				real dz = p[j].r.z - p[i].r.z;
-				real rr = dx * dx + dy * dy + dz * dz + eps;
-				real f  = p[j].m / (rr * sqrt(rr));
-				a[i].x += f * dx;
-				a[i].y += f * dy;
-				a[i].z += f * dz;
-			}
-		}
-
-		for(size_t i = 0; i < n; ++i) {
-			p[i].r.x += dt * p[i].u.x;
-			p[i].r.y += dt * p[i].u.y;
-			p[i].r.z += dt * p[i].u.z;
-			p[i].u.x += dt * a[i]  .x;
-			p[i].u.y += dt * a[i]  .y;
-			p[i].u.z += dt * a[i]  .z;
-		}
+		acceleration(a, d->p, d->n);
+		forwardEuler(d->p, a, d->n, dt);
 		output(stdout, d, argv[0], dt);
 	}
 	free(a);
