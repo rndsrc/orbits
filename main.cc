4ff720f18f4056293d3522f0a53b43aa1362937e
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-08 12:24:08 -0400

    Print usage as long as the time step is not set to a positive number

--- main.cc
+++ main.cc
@@ -6,17 +6,16 @@
 
 int main(int argc, char **argv)
 {
-	if(argc > 1 && !strcmp(argv[1], "--help")) {
+	real dt = argc > 1 ? atof(argv[1]) : 0.0;
+
+	if(dt <= 0.0) {
 		printf("usage: %s [step_size] < input_file\n", argv[0]);
 		return 0;
 	}
-
-	real dt = argc > 1 ? atof(argv[1]) : 1e-3;
 	printf("dt = %g\n", dt);
 
 	if(isatty(fileno(stdin)))
 		printf("Please input initial conditions [end by Ctr+D]:\n");
-
 	char line[1024];
 	while(fgets(line, sizeof(line), stdin))
 		if(line[0] != '#')
