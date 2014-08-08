e39392989421f0dbb09560ec2da2327eaf52ca48
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-08 12:36:51 -0400

    Parse input into particle states

--- main.cc
+++ main.cc
@@ -14,12 +14,26 @@ int main(int argc, char **argv)
 	}
 	printf("dt = %g\n", dt);
 
+	size_t    n = 0, m = 64;
+	particle *p = (particle *)malloc(sizeof(particle) * m);
+
 	if(isatty(fileno(stdin)))
 		printf("Please input initial conditions [end by Ctr+D]:\n");
 	char line[1024];
-	while(fgets(line, sizeof(line), stdin))
-		if(line[0] != '#')
+	while(fgets(line, sizeof(line), stdin)) {
+		if(line[0] == '#')
+			continue;
+		else
 			fputs(line, stdout);
+
+		if(n >= m)
+			p = (particle *)realloc(p, sizeof(particle) * (m *= 2));
+		sscanf(line, "%lf %lf %lf %lf %lf %lf %lf",
+		       &p[n].m,
+		       &p[n].r.x, &p[n].r.y, &p[n].r.z,
+		       &p[n].u.x, &p[n].u.y, &p[n].u.z);
+		++n;
+	}
 
 	return 0;
 }
