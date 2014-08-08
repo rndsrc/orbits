3368b2b42df177cb3a361582e73deafeb1487125
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-08 14:17:35 -0400

    Implement a foward Euler scheme

--- main.cc
+++ main.cc
@@ -1,5 +1,8 @@
 #include "orbits.h"
 #include <cstdlib>
+#include <cmath>
+
+const real eps = 1.0e-32;
 
 int main(int argc, char **argv)
 {
@@ -11,7 +14,37 @@ int main(int argc, char **argv)
 
 	FILE *f = argc > 2 ? fopen(argv[argc-1], "r") : NULL;
 	data *d = input(f ? f : stdin);
-	output(stdout, d, argv[0], dt);
+
+	vector *a = (vector *)malloc(sizeof(vector) * d->n);
+	for(size_t h = 0; h < 6283; ++h) {
+		size_t    n = d->n;
+		particle *p = d->p;
+
+		for(size_t i = 0; i < n; ++i) {
+			a[i].z = a[i].y = a[i].x = 0.0;
+			for(size_t j = 0; j < n; ++j) {
+				real dx = p[j].r.x - p[i].r.x;
+				real dy = p[j].r.y - p[i].r.y;
+				real dz = p[j].r.z - p[i].r.z;
+				real rr = dx * dx + dy * dy + dz * dz + eps;
+				real f  = p[j].m / (rr * sqrt(rr));
+				a[i].x += f * dx;
+				a[i].y += f * dy;
+				a[i].z += f * dz;
+			}
+		}
+
+		for(size_t i = 0; i < n; ++i) {
+			p[i].r.x += dt * p[i].u.x;
+			p[i].r.y += dt * p[i].u.y;
+			p[i].r.z += dt * p[i].u.z;
+			p[i].u.x += dt * a[i]  .x;
+			p[i].u.y += dt * a[i]  .y;
+			p[i].u.z += dt * a[i]  .z;
+		}
+		output(stdout, d, argv[0], dt);
+	}
+	free(a);
 
 	return 0;
 }
