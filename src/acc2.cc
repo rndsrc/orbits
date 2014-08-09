80be47388a3bf469b4a84c18883c01147ce49755
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-08 22:54:17 -0400

    The epsilon trick doesn't work: skip particle self-interaction

--- src/acc2.cc
+++ src/acc2.cc
@@ -3,19 +3,18 @@
 
 void acc2(vector *a, const particle *p, size_t n, real dt)
 {
-	const real eps   = 1.0e-32;
-	const real dt224 = dt * dt / 24.0;
+	const real h = dt * dt / 24.0;
 
 	for(size_t i = 0; i < n; ++i) {
 		real ax  = 0.0, ay  = 0.0, az  = 0.0;
 		real axx = 0.0, ayy = 0.0, azz = 0.0;
 		real axy = 0.0, ayz = 0.0, azx = 0.0;
 
-		for(size_t j = 0; j < n; ++j) {
+		for(size_t j = 0; j < n; ++j) if(j != i) {
 			real dx = p[j].r.x - p[i].r.x;
 			real dy = p[j].r.y - p[i].r.y;
 			real dz = p[j].r.z - p[i].r.z;
-			real rr = dx * dx + dy * dy + dz * dz + eps;
+			real rr = dx * dx + dy * dy + dz * dz;
 
 			real f  = p[j].m / (rr * sqrt(rr));
 			real g  = -3.0 * f / rr;
@@ -33,8 +32,8 @@ void acc2(vector *a, const particle *p, size_t n, real dt)
 			azx += g * dz * dx;
 		}
 
-		a[i].x = ax + dt224 * (ax * axx + ay * axy + az * azx);
-		a[i].y = ay + dt224 * (ax * axy + ay * ayy + az * ayz);
-		a[i].z = az + dt224 * (ax * azx + ay * ayz + az * azz);
+		a[i].x = ax + h * (ax * axx + ay * axy + az * azx);
+		a[i].y = ay + h * (ax * axy + ay * ayy + az * ayz);
+		a[i].z = az + h * (ax * azx + ay * ayz + az * azz);
 	}
 }
