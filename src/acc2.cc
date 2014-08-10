dc19296e7df7ffc271668bfd7ec2bf08c83b7ad9
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-09 23:39:47 -0400

    Optimize acc2() to reduce computation

--- src/acc2.cc
+++ src/acc2.cc
@@ -13,24 +13,19 @@ void acc2(vector *a, vector *b, const particle *p, size_t n, real dt)
 			real dy = p[j].r.y - p[i].r.y;
 			real dz = p[j].r.z - p[i].r.z;
 			real rr = dx * dx + dy * dy + dz * dz;
-			real f  = p[j].m / (rr * sqrt(rr));
+			real f  = p[j].m / (rr * rr * sqrt(rr));
 			real fx = f * (a[j].x - a[i].x);
 			real fy = f * (a[j].y - a[i].y);
 			real fz = f * (a[j].z - a[i].z);
-
-			b[i].x += fx * (3 * dx * dx / rr - 1)
-				+ fy * (3 * dy * dx / rr    )
-				+ fz * (3 * dz * dx / rr    );
-			b[i].y += fx * (3 * dx * dy / rr    )
-				+ fy * (3 * dy * dy / rr - 1)
-				+ fz * (3 * dz * dy / rr    );
-			b[i].z += fx * (3 * dx * dz / rr    )
-				+ fy * (3 * dy * dz / rr    )
-				+ fz * (3 * dz * dz / rr - 1);
+			real df = dx * fx + dy * fy + dz * fz;
+			real rr3= rr / 3;
+			b[i].x += dx * df - fx * rr3;
+			b[i].y += dy * df - fy * rr3;
+			b[i].x += dz * df - fz * rr3;
 		}
 	}
 
-	real h = -dt * dt / 24.0;
+	real h = -dt * dt / 8.0;
 	for(size_t i = 0; i < n; ++i) {
 		a[i].x += h * b[i].x;
 		a[i].y += h * b[i].y;
