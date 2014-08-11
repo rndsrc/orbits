993087f250acaa59beeb37ec20db12eb860cd590
Chi-kwan Chan <chanc@email.arizona.edu>
2014-08-10 20:15:25 -0400

    Optimize acc() by using symmetries of the acceleration terms

--- src/acc.cc
+++ src/acc.cc
@@ -21,17 +21,23 @@
 
 void acc(vector *a, const particle *p, size_t n)
 {
-	for(size_t i = 0; i < n; ++i) {
+	for(size_t i = 0; i < n; ++i)
 		a[i].z = a[i].y = a[i].x = 0.0;
-		for(size_t j = 0; j < n; ++j) if(j != i) {
+
+	for(size_t i = 0; i < n; ++i)
+		for(size_t j = 0; j < i; ++j) {
 			real dx = p[j].r.x - p[i].r.x;
 			real dy = p[j].r.y - p[i].r.y;
 			real dz = p[j].r.z - p[i].r.z;
 			real rr = dx * dx + dy * dy + dz * dz;
-			real f  = p[j].m / (rr * sqrt(rr));
-			a[i].x += f * dx;
-			a[i].y += f * dy;
-			a[i].z += f * dz;
-		} // 16 FLOP + 1 sqrt()
-	}
+			real f  = 1.0 / (rr * sqrt(rr));
+			real fi = f * p[i].m;
+			real fj = f * p[j].m;
+			a[i].x += fj * dx;
+			a[i].y += fj * dy;
+			a[i].z += fj * dz;
+			a[j].x -= fi * dx;
+			a[j].y -= fi * dy;
+			a[j].z -= fi * dz;
+		} // 24 FLOP + 1 sqrt()
 }
